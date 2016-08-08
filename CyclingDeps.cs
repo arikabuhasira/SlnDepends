cae7f200079fd6b56dc082380ceefa932bdcbba1
Arik.Abuhasira <arik.abuhasira@zerto.com>
2016-08-08 10:47:43 +0300

    initial revision

---
+++ CyclingDeps.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+// ReSharper disable InconsistentNaming
+
+namespace System.Collections.Extensions
+{
+    /**
+     * Find cycling dependency chains.
+     * 
+     * This is an extension for dictionary, since we use as data structure to represent a graph. 
+     * Dictionary<T, TValueList> . key = node, value list - neighbours
+     *  
+     *  e.g: 
+     *  Dictionary<strin, List<string> = {
+     *      { "a", [ "b", "c"] },
+     *      { "b", [ "d", "e", "f"] },
+     *      { "e", [ "g", "h", "i"] },
+     *      { "g", [ "a"] },
+     *      { "i", [ "b"] },
+     *  }
+     *  
+     *  cyclic: 
+     *  a.b.e.g.a
+     *  b.e.i.b
+     *  
+     */
+    public static class CyclingDeps
+    {
+        public static IEnumerable<IEnumerable<T>> FindCycles<T, TList>(this IDictionary<T, TList> graph)
+            where TList : class, IEnumerable<T>    
+            where T : IEquatable<T>                
+        {
+            var cycles = new List<CyclePath<T>>();
+            var visitors = new Visitors<T>();
+            var edges = new Func<T, IEnumerable<T>>(key => graph.ValueOrDefault(key, null) ?? Enumerable.Empty<T>());
+            var graphNodes = graph.Keys;
+
+            foreach (var node in graphNodes)
+            {
+                var foundCycles = FindCycles(node, edges, visitors);
+                cycles.AddRange(foundCycles);   
+            }
+
+            return cycles;
+        }
+
+        class CyclePath<T> : List<T> { }
+        class CycleStack<T> : Stack<KeyValuePair<T, IEnumerator<T>>> { }   
+        class Visitors<T> : Dictionary<T, VisitState> { }
+
+        enum VisitState { NotVisited, Visiting, Visited };
+
+        static List<CyclePath<T>> FindCycles<T>(T root, Func<T, IEnumerable<T>> edges, Visitors<T> visitors)
+            where T : IEquatable<T>
+        {
+            var cycles = new List<CyclePath<T>>();
+            var stack  = new CycleStack<T>();
+
+            Push(stack, root, edges, visitors, cycles);  // push the first
+            while (stack.Count > 0)
+            {
+                var pair = stack.Peek();
+                var it = pair.Value;   // current graph node edges iterator
+                var node = pair.Key;
+
+                if (it.MoveNext()) Push(stack, it.Current, edges, visitors, cycles);
+                else { stack.Pop(); visitors[node] = VisitState.Visited; }
+            }
+            return cycles;
+        }
+
+        static void Push<T>(                        
+            CycleStack<T> stack,                  // implement non-recursive 
+            T node,                               // graph node
+            Func<T, IEnumerable<T>> edges,        // graph edges.
+            Visitors<T> visitors,                 // graph coloring with visited, visiting and not visited
+            List<CyclePath<T>> cycles,            // aggregate found cycles
+            IEqualityComparer<T> comparer = null  // graph node comparer (e.g: for strings - ignoredcase)
+            ) where T : IEquatable<T>
+        {
+            var visit = visitors.ValueOrDefault(node, VisitState.NotVisited);
+            switch (visit)
+            {
+                case VisitState.Visited: return;
+                case VisitState.Visiting:
+                    if (stack.Count == 0) throw new InvalidOperationException("oops. stack is empty");
+
+                    var cmp = comparer ?? EqualityComparer<T>.Default;
+                    var path = new CyclePath<T>();
+
+                    var it = stack.GetEnumerator();// climb 
+                    while (it.MoveNext() && !cmp.Equals(it.Current.Key, node))
+                        path.Add(it.Current.Key);
+                    cycles.Add(path);
+                    break;
+                case VisitState.NotVisited:
+                    visitors[node] = VisitState.Visiting;
+                    stack.Push(new KeyValuePair<T, IEnumerator<T>>(node, edges(node).GetEnumerator()));
+                    break;
+            }
+        }
+        
+        /** get dictionary value OR default one **/
+        static V ValueOrDefault<K, V>(this IDictionary<K, V> dictionary, K key, V defaultValue)
+        {
+            V value;
+            return dictionary.TryGetValue(key, out value) ? value : defaultValue;
+        }
+    }
+
+
+    class Tests
+    {
+        private static string test1 = @"a->b;b->c;c->d";
+        private static string test2 = @"a->b;b->c;c->a";
+        private static string test3 = @"a->b,c,d; b->e,c; c->e,f; f->a;";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿// ReSharper disable InconsistentNaming
+using System;
+using System.Collections.Extensions;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using rx = System.Text.RegularExpressions.Regex;
+using Edges = System.Collections.Generic.List<string>;
+using Graph = System.Collections.Generic.Dictionary<string, System.Collections.Generic.IEnumerable<string>>;
+
+namespace SlnDepends
+{
+    public class VsDependsException : Exception
+    {
+        public VsDependsException(string msg) : base(msg) { }
+        public static implicit operator VsDependsException(string msg) => new VsDependsException(msg);
+    }
+
+    static class Program
+    {
+        static void printf(string msg) => Console.WriteLine(msg);
+        static void die   (string msg, Exception e = null) 
+        {
+            printf("CRITICAL:" + msg);
+            if (e != null) printf($"EXCEPTION:\n{e}");   // print exception
+            Environment.Exit(1);
+        }
+
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += (sender, eventArgs) => die("unhandle exception", (eventArgs.ExceptionObject as Exception));
+
+            if (args.Length != 1) die("Missing sln file argument.");
+            if (rx.IsMatch(args[0], @"(?i)^(-h|--help|\\\?)$")) Usage();   // print help with -h or --help or /?
+
+            var sln = args[0];
+            var sln_dir = Path.GetDirectoryName(sln);
+
+            var sln_content = ReadFile(sln).AsSingleLine();
+            var cs_projects = ParseSlnProjects(sln_content);
+            if (cs_projects.Length == 0) die("zero projects found in sln");
+
+            printf($"sln contains {cs_projects.Length}");
+
+            printf("== build graph:");
+            Graph sln_graph = new Graph(StringComparer.InvariantCultureIgnoreCase);
+            foreach (var csprojFilename in cs_projects)
+            {
+                printf($"> process {csprojFilename}");
+
+                var csprojAbsFilename = Path.Combine(sln_dir, csprojFilename);   // resolve project full path
+                var csproj = ReadFile(csprojAbsFilename).AsSingleLine();         // read project
+                var assembly = ParseProjectAssemblyName(csproj);                 // get project assembly
+                var references = ParseProjectRefs(csproj);                       // get project references
+                var edges = string.Join(";", references);                        // dbg: concat references
+
+                printf($"assembly={assembly}");
+                printf($"edges ({references.Length})={edges}");
+
+                sln_graph.Add(assembly /* Node */, references /* Edges */);
+            }
+            printf("== end of graph");
+
+            printf("=== finding cycles ====");
+            var cycles = sln_graph.FindCycles();
+            printf($"cycles={cycles.Count()}");
+        }
+
+        static void Usage()
+        {
+            printf("usage: slncyclic [FILE]");
+            printf("Find cyclic dependecies within VS solution file");
+            Environment.Exit(1); 
+        }
+
+        static string ReadFile(string filename)
+        {
+            if (!File.Exists(filename)) die($"file not exists. fail to read {filename}.");
+            using (var reader = File.OpenText(filename))
+                return reader.ReadToEnd();
+        }
+
+        static string[] ParseSlnProjects(string content)
+        {
+            var matches = rx.Matches(content, @"(?mi)([a-zA-Z\.\\\-0-9_]+csproj)"); // FIXME: remove multiline option ?m
+            var csprojList = new List<string>();
+            for (var i=0; i<matches.Count;i++) csprojList.Add(matches[i].Value);
+            return csprojList.ToArray();
+        }
+
+        static string[] ParseProjectRefs(string content)
+        {
+            var matches = rx.Matches(content, @"(?mis)(<hintpath>(?<dll>.*?)<\/hintpath>|projectref.+?<name>(?<dll>.+?)<\/name>)");
+
+            var csprojDlls = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
+            for (var i = 0; i < matches.Count; i++)
+            {
+                var dllNameFull = matches[i].Groups["dll"].Value;                       // with extension and path
+                var dllName = rx.Replace(dllNameFull, @"^(.+)\\(.+?)\.dll$", "$2");     // bare name (without extension + path)
+                if (!csprojDlls.Contains(dllName)) csprojDlls.Add(dllName);
+            }
+
+            return csprojDlls.ToArray();
+        }
+
+        static string ParseProjectAssemblyName(string content)
+        {
+            var m = rx.Match(content, @"(?i)<assemblyname>(?<output>.+?)<\/assemblyname>");
+            if (!m.Success) throw (VsDependsException) "fail to parse assembly name";
+            return m.Groups["output"].Value;
+        }
+
+        private static string AsSingleLine(this string text) => text.Replace("\r\n", "");
+    }
+}
